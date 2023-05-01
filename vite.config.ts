3924552ff1574e9e54c9548f90d183326cd7e1e7
BHyeonKim <rlaqhguse@gmail.com>
2023-05-02 04:42:50 +0900

    chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr({exclude:'/public'})],
+  resolve:{
+    alias:{
+      'src': path.resolve(__dirname,'./src')
+    },
+  }
 });
