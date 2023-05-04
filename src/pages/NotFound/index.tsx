abff03ea31c5ffa8fa1d422be42f51a910a20d79
BHyeonKim <rlaqhguse@gmail.com>
2023-05-05 02:02:38 +0900

    feat(notfoundpage): 404페이지 작성

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind'
+import FullScreen from 'src/components/layout/FullScreen'
+
+import styles from './notFound.module.scss'
+
+const cx = classNames.bind(styles)
+
 const NotFoundPage = () => {
-  return <div>not found</div>
+  return (
+    <FullScreen className={cx('center')}>
+      <h1 className={cx('title')}>404</h1>
+      <p>Something went Wrong</p>
+    </FullScreen>
+  )
 }
 
 export default NotFoundPage
