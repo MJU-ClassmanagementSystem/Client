0a2a16f8de0c1940124f87ac8ae574cbe8f22f6b
BHyeonKim <rlaqhguse@gmail.com>
2023-05-05 15:41:59 +0900

    fix: login mocking 핸들러 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -8,12 +8,7 @@ export const handlers = [
       return res(
         ctx.status(401),
         ctx.json({
-          success: false,
           data: null,
-          error: {
-            code: '401',
-            message: '잘못된 ACCESS TOKEN 입니다.',
-          },
         }),
       )
 
@@ -21,11 +16,8 @@ export const handlers = [
       ctx.delay(100),
       ctx.status(200),
       ctx.json({
-        success: true,
-        data: {
-          accessToken:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0IiwiaWF0IjoxNjY2NjcwNDQ0LCJleHAiOjE2NjY3NTY4NDR9.G2o4ffsAtGeJvRNTT7RPJCSeKoKNcaBKJ4Tm5WuBZWg',
-        },
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0IiwiaWF0IjoxNjY2NjcwNDQ0LCJleHAiOjE2NjY3NTY4NDR9.G2o4ffsAtGeJvRNTT7RPJCSeKoKNcaBKJ4Tm5WuBZWg',
       }),
     )
   }),
