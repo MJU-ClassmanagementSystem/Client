194c88fabd60957e2e352016d36a1bef7fbc3856
BHyeonKim <rlaqhguse@gmail.com>
2023-05-08 03:21:49 +0900

    feat(signuppage): 회원가입페이지 작성

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind'
-import splashImage from 'src/assets/images/splashImage.jpg'
+import { useState } from 'react'
+import Dropdown from 'src/components/features/Dropdown'
 import FullScreen from 'src/components/layout/FullScreen'
 
 import styles from './signUp.module.scss'
@@ -7,21 +8,83 @@ import styles from './signUp.module.scss'
 const cx = classNames.bind(styles)
 
 const SignUpPage = () => {
+  const [selectedItem, setSelectedItem] = useState('선생님')
+
+  const isTeacher = selectedItem === '선생님'
+  const isParent = !isTeacher
+
   return (
     <FullScreen className={cx('signUp')}>
-      <img className={cx('photo')} alt="사진" src={splashImage} />
+      <div className={cx('photo')}>
+        <p>Class Management System</p>
+      </div>
       <div className={cx('formContainer')}>
         <form className={cx('form')}>
           <h1 className={cx('title')}>Sign Up</h1>
-          <label htmlFor="id">id</label>
-          <input id="id" type="text" />
-          <label htmlFor="password">password</label>
-          <input id="password" type="password" />
-          <label htmlFor="name">name</label>
-          <input id="name" type="text" />
-          <label htmlFor="school">school</label>
-          <input id="school" type="text" />
-          <button>회원가입</button>
+          <Dropdown
+            title="type"
+            itemList={['선생님', '학부모']}
+            value={selectedItem}
+            onClick={setSelectedItem}
+          />
+          <label className={cx('label')} htmlFor="id">
+            id
+          </label>
+          <input
+            className={cx('input')}
+            id="id"
+            type="text"
+            placeholder="아이디를 입력해주세요"
+          />
+          <label className={cx('label')} htmlFor="password">
+            password
+          </label>
+          <input
+            className={cx('input')}
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+          {isParent && (
+            <>
+              <label className={cx('label')} htmlFor="text">
+                studentId
+              </label>
+              <input
+                className={cx('input')}
+                id="text"
+                type="text"
+                placeholder="자녀의 학번을 입력해주세요"
+              />
+            </>
+          )}
+          {isTeacher && (
+            <>
+              <label className={cx('label')} htmlFor="name">
+                name
+              </label>
+              <input
+                className={cx('input')}
+                id="name"
+                type="text"
+                placeholder="성함을 입력해주세요"
+              />
+            </>
+          )}
+          {isTeacher && (
+            <>
+              <label className={cx('label')} htmlFor="school">
+                school
+              </label>
+              <input
+                className={cx('input')}
+                id="school"
+                type="text"
+                placeholder="소속된 학교를 입력해주세요"
+              />
+            </>
+          )}
+          <button className={cx('button')}>회원가입</button>
         </form>
       </div>
     </FullScreen>
