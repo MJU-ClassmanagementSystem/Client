eeb895fa4f5c614f0a615bf11b929a329b6063cf
BHyeonKim <rlaqhguse@gmail.com>
2023-05-08 04:26:20 +0900

    feat(signuppage): 사용자 입력 폼 저장 및 전송
    사용자 입력 폼 저장은 추후 리팩토링이 필요합니다. 전송과정에서도 에러핸들링을 하고 있지 않으므로 추후 수정이 필요합니다.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames/bind'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import Dropdown from 'src/components/features/Dropdown'
 import FullScreen from 'src/components/layout/FullScreen'
+import classManagement from 'src/service/classManagement'
 
 import styles from './signUp.module.scss'
 
@@ -9,9 +10,32 @@ const cx = classNames.bind(styles)
 
 const SignUpPage = () => {
   const [selectedItem, setSelectedItem] = useState('선생님')
+  // TODO: input 리펙토링
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+  const [studentId, setStudentId] = useState('')
+  const [name, setName] = useState('')
+  const [school, setSchool] = useState('')
 
   const isTeacher = selectedItem === '선생님'
   const isParent = !isTeacher
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    // TODO: 에러 핸들링
+    switch (selectedItem) {
+      case '선생님':
+        if (id && password && name && school)
+          classManagement.signupTeacher(id, password, name, school)
+        return
+      case '학부모':
+        if (id && password && studentId)
+          classManagement.signupParent(id, password, studentId)
+        return
+      default:
+        return
+    }
+  }
 
   return (
     <FullScreen className={cx('signUp')}>
@@ -19,7 +43,7 @@ const SignUpPage = () => {
         <p>Class Management System</p>
       </div>
       <div className={cx('formContainer')}>
-        <form className={cx('form')}>
+        <form className={cx('form')} onSubmit={handleSubmit}>
           <h1 className={cx('title')}>Sign Up</h1>
           <Dropdown
             title="type"
@@ -34,7 +58,9 @@ const SignUpPage = () => {
             className={cx('input')}
             id="id"
             type="text"
+            value={id}
             placeholder="아이디를 입력해주세요"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setId(e.currentTarget.value)}
           />
           <label className={cx('label')} htmlFor="password">
             password
@@ -43,7 +69,11 @@ const SignUpPage = () => {
             className={cx('input')}
             id="password"
             type="password"
+            value={password}
             placeholder="비밀번호를 입력해주세요"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.currentTarget.value)
+            }
           />
           {isParent && (
             <>
@@ -54,7 +84,11 @@ const SignUpPage = () => {
                 className={cx('input')}
                 id="text"
                 type="text"
+                value={studentId}
                 placeholder="자녀의 학번을 입력해주세요"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setStudentId(e.currentTarget.value)
+                }
               />
             </>
           )}
@@ -67,7 +101,11 @@ const SignUpPage = () => {
                 className={cx('input')}
                 id="name"
                 type="text"
+                value={name}
                 placeholder="성함을 입력해주세요"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setName(e.currentTarget.value)
+                }
               />
             </>
           )}
@@ -80,11 +118,17 @@ const SignUpPage = () => {
                 className={cx('input')}
                 id="school"
                 type="text"
+                value={school}
                 placeholder="소속된 학교를 입력해주세요"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setSchool(e.currentTarget.value)
+                }
               />
             </>
           )}
-          <button className={cx('button')}>회원가입</button>
+          <button className={cx('button')} type="submit">
+            회원가입
+          </button>
         </form>
       </div>
     </FullScreen>
