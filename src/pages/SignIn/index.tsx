f68a0cf848837831469406c7c4252056bade0def
BHyeonKim <rlaqhguse@gmail.com>
2023-05-08 23:15:19 +0900

    style(signinpage): 이미지 추가 및 스타일 수정

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,55 @@
+import { DevTool } from '@hookform/devtools'
+import classNames from 'classnames/bind'
+import { useForm } from 'react-hook-form'
+import FullScreen from 'src/components/layout/FullScreen'
+import { SignInFormData } from 'src/types/reactHookForm'
+
 import styles from './signIn.module.scss'
 
+const cx = classNames.bind(styles)
+
 const SignInPage = () => {
+  const { register, control, handleSubmit } = useForm<SignInFormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
-    <div className={styles.wrap}>
-      <div className={styles.imgWrap} />
-      <div className={styles.contentWrap}>
-        <form>
-          <h1 className={styles.title}>Sign In</h1>
-          <div className={styles.inpWrap}>
-            <p>ID</p>
-            <input type="text" />
+    <FullScreen className={cx('wrap')}>
+      <div className={cx('imgWrap')}>
+        <p>Class Management System</p>
+      </div>
+      <div className={cx('contentWrap')}>
+        <form onSubmit={onSubmit}>
+          <h1 className={cx('title')}>Sign In</h1>
+          <div className={cx('inpWrap')}>
+            <label htmlFor="id" className={cx('label')}>
+              ID
+            </label>
+            <input
+              id="id"
+              type="text"
+              {...register('id')}
+              placeholder="아이디를 입력해주세요"
+            />
           </div>
-          <div className={styles.inpWrap}>
-            <p>PW</p>
-            <input type="text" />
+          <div className={cx('inpWrap')}>
+            <label htmlFor="password" className={cx('label')}>
+              Passwrod
+            </label>
+            <input
+              id="password"
+              type="text"
+              {...register('password')}
+              placeholder="비밀번호를 입력해주세요"
+            />
           </div>
-          <button className={styles.submitBtn}>Sign In</button>
+          <button className={cx('submitBtn')}>Sign In</button>
         </form>
       </div>
-    </div>
+      <DevTool control={control} />
+    </FullScreen>
   )
 }
 
---
+++ src/types/reactHookForm.ts
@@ -0,0 +1,4 @@
+export type SignInFormData = {
+  id: string
+  password: string
+}
