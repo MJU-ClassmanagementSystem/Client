7b7a95732a126b92893eb52079cc78c18b170ae8
snowari <pes1149@naver.com>
2023-05-08 22:52:17 +0900

    feat(loginpage): 로그인 페이지

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,25 @@
+import styles from './signIn.module.scss'
+
 const SignInPage = () => {
-  return <div>sign in</div>
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.imgWrap} />
+      <div className={styles.contentWrap}>
+        <form>
+          <h1 className={styles.title}>Sign In</h1>
+          <div className={styles.inpWrap}>
+            <p>ID</p>
+            <input type="text" />
+          </div>
+          <div className={styles.inpWrap}>
+            <p>PW</p>
+            <input type="text" />
+          </div>
+          <button className={styles.submitBtn}>Sign In</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default SignInPage
