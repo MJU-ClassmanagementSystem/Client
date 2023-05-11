fe019524c92c843ff5cddd8b34d67a78c8dbea89
BHyeonKim <rlaqhguse@gmail.com>
2023-05-12 03:49:35 +0900

    feat: 버튼 클릭시 week 증가 혹은 감소 구현

--- src/components/features/WeekNavigator/index.tsx
+++ src/components/features/WeekNavigator/index.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { ArrowLeftIcon, ArrowRightIcon } from 'src/assets/svgs'
 
 import styles from './weekNavigator.module.scss'
@@ -6,13 +7,30 @@ import styles from './weekNavigator.module.scss'
 const cx = classNames.bind(styles)
 
 const WeekNavigator = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  const { week } = useParams()
+
+  const handleToPrevWeek = () => {
+    if (!week) return
+    const newUrl = pathname.replace(week, String(Number(week) + 1))
+    navigate(newUrl)
+  }
+
+  const handleToNextWeek = () => {
+    if (!week) return
+    if (Number(week) <= 1) return
+    const newUrl = pathname.replace(week, String(Number(week) - 1))
+    navigate(newUrl)
+  }
+
   return (
     <div className={cx('navigator')}>
-      <button>
+      <button onClick={handleToPrevWeek}>
         <ArrowLeftIcon />
       </button>
       <h2 className={cx('week')}>4월 첫째 주</h2>
-      <button>
+      <button onClick={handleToNextWeek}>
         <ArrowRightIcon />
       </button>
     </div>
