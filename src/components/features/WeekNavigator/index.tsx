6e744955fc777e3e2ab23ce1fabca99f20791808
BHyeonKim <rlaqhguse@gmail.com>
2023-05-12 19:06:02 +0900

    feat(header): 주차 조작 버튼으로 주차 조작시 path 변경
    헤더 컴포넌트의 버튼으로 주차를 앞뒤로 변경시 path를 변경하고 헤더 컴포넌트의 타이틀도 그 월과 주차에 맞게 변경할 수 있게 했습니다.

--- src/components/features/WeekNavigator/index.tsx
+++ src/components/features/WeekNavigator/index.tsx
@@ -1,19 +1,32 @@
 import classNames from 'classnames/bind'
+import { useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { ArrowLeftIcon, ArrowRightIcon } from 'src/assets/svgs'
+import { getWeekNumber } from 'src/utils/week'
 
 import styles from './weekNavigator.module.scss'
 
 const cx = classNames.bind(styles)
 
+const todayDate = new Date()
+
 const WeekNavigator = () => {
   const navigate = useNavigate()
-  const { pathname } = useLocation()
   const { week } = useParams()
+  const [date, setDate] = useState<Date>(
+    new Date(
+      todayDate.getFullYear(),
+      todayDate.getMonth(),
+      todayDate.getDate() - (Number(week) - 1) * 7,
+    ),
+  )
+  const { pathname } = useLocation()
+  const { formattedString } = getWeekNumber(date)
 
   const handleToPrevWeek = () => {
     if (!week) return
     const newUrl = pathname.replace(week, String(Number(week) + 1))
+    setDate((date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() - 7))
     navigate(newUrl)
   }
 
@@ -21,6 +34,8 @@ const WeekNavigator = () => {
     if (!week) return
     if (Number(week) <= 1) return
     const newUrl = pathname.replace(week, String(Number(week) - 1))
+    setDate((date) => new Date(date.getFullYear(), date.getMonth(), date.getDate() + 7))
+
     navigate(newUrl)
   }
 
@@ -29,7 +44,7 @@ const WeekNavigator = () => {
       <button onClick={handleToPrevWeek}>
         <ArrowLeftIcon />
       </button>
-      <h2 className={cx('week')}>4월 첫째 주</h2>
+      <h2 className={cx('week')}>{formattedString}</h2>
       <button onClick={handleToNextWeek}>
         <ArrowRightIcon />
       </button>
