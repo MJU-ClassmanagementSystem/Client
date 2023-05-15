cbb05e99980ef0bc0ea53cb98ee8171fd4b4fb81
snowari <pes1149@naver.com>
2023-05-16 00:21:01 +0900

    style(welcomepage): 폰트 사이즈, 간격 수정

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -21,33 +21,35 @@ const WelcomePage = () => {
           <span className={cx('coloredTitle')}>MJU</span> Class Management
         </h1>
       </div>
-      <div className={cx('welcomePage')}>
-        <div className={cx('leftWrap')}>
-          <div className={cx('contentsWrap')}>
-            <div className={cx('textWrap')}>
-              <h2>
-                <span className={cx('boldText')}>수업 태도 분석</span>과
-                <br />
-                <span className={cx('boldText')}>학교 생활 지도</span>를
-                <br /> 한번에.
-              </h2>
-              <p>
-                AI와 카메라를 통해
-                <br /> 아이의 수업 태도와 학교생활 데이터를 수집하고 분석하여
-                <br /> 어디서든 아이의 수업 집중도와 흥미도를 관찰해 보세요.
-              </p>
+      <div className={cx('pageWrap')}>
+        <div className={cx('welcomePage')}>
+          <div className={cx('leftWrap')}>
+            <div className={cx('contentsWrap')}>
+              <div className={cx('textWrap')}>
+                <h2>
+                  <span className={cx('boldText')}>수업 태도 분석</span>과
+                  <br />
+                  <span className={cx('boldText')}>학교 생활 지도</span>를
+                  <br /> 한번에.
+                </h2>
+                <p>
+                  AI와 카메라를 통해
+                  <br /> 아이의 수업 태도와 학교생활 데이터를 수집하고 분석하여
+                  <br /> 어디서든 아이의 수업 집중도와 흥미도를 관찰해 보세요.
+                </p>
+              </div>
+              <div className={cx('buttonWrap')}>
+                <LoginButton text="선생님으로 로그인" url="/manageStudent" />
+                <LoginButton text="학부모로 로그인" url="/manageStudent" />
+              </div>
+              <button className={cx('signUp')} onClick={handleSignInClick}>
+                Sign up...
+              </button>
             </div>
-            <div className={cx('buttonWrap')}>
-              <LoginButton text="선생님으로 로그인" url="/manageStudent" />
-              <LoginButton text="학부모로 로그인" url="/manageStudent" />
-            </div>
-            <button className={cx('signUp')} onClick={handleSignInClick}>
-              Sign up...
-            </button>
           </div>
-        </div>
-        <div className={cx('imgWrap')}>
-          <img src={welcomeImg} alt="img" />
+          <div className={cx('imgWrap')}>
+            <img src={welcomeImg} alt="img" />
+          </div>
         </div>
       </div>
     </>
