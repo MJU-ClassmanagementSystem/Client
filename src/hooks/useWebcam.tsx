5f0725f676966306682ce95e3a779333eabb1e2b
BHyeonKim <rlaqhguse@gmail.com>
2023-05-17 20:06:44 +0900

    feat(webcam): 브라우저의 화면에 나온 웹캠의 프레임을 가져오기

--- src/hooks/useWebcam.tsx
+++ src/hooks/useWebcam.tsx
@@ -1,17 +1,22 @@
-import { RefObject, useEffect, useRef } from 'react'
+import { MutableRefObject, RefObject, useEffect, useRef, useState } from 'react'
 
 export const captureFrame = (
+  animationIdRef: MutableRefObject<number | undefined>,
   videoRef: RefObject<HTMLVideoElement>,
   canvas: HTMLCanvasElement,
+  isCapturing: boolean,
 ): number | undefined => {
-  if (!videoRef.current || !canvas) return
+  if (!isCapturing || !videoRef.current || !canvas) return
   const context = canvas.getContext('2d')
   context?.drawImage(videoRef.current, 0, 0)
   const imageData = canvas.toDataURL('image/jpeg')
   console.log(imageData)
-  return requestAnimationFrame(() => captureFrame(videoRef, canvas))
+  animationIdRef.current = requestAnimationFrame(() =>
+    captureFrame(animationIdRef, videoRef, canvas, isCapturing),
+  )
 }
 
+// 브라우저에 비디오 화면을 그리는 함수
 const startVideo = async (videoRef: RefObject<HTMLVideoElement>) => {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true })
   if (!videoRef || !videoRef.current || videoRef.current.srcObject) return
@@ -21,12 +26,29 @@ const startVideo = async (videoRef: RefObject<HTMLVideoElement>) => {
 
 const useWebcam = (videoRef: RefObject<HTMLVideoElement>) => {
   const canvasRef = useRef<HTMLCanvasElement>(document.createElement('canvas'))
+  const animationFrameId = useRef<number>()
+  const [isCapturing, setIsCapturing] = useState(false)
 
   useEffect(() => {
     startVideo(videoRef)
 
-    captureFrame(videoRef, canvasRef.current)
-  }, [videoRef])
+    if (isCapturing) {
+      animationFrameId.current = captureFrame(
+        animationFrameId,
+        videoRef,
+        canvasRef.current,
+        isCapturing,
+      )
+    } else {
+      if (animationFrameId.current) cancelAnimationFrame(animationFrameId.current)
+    }
+
+    return () => {
+      if (animationFrameId.current) cancelAnimationFrame(animationFrameId.current)
+    }
+  }, [isCapturing, videoRef])
+
+  return { setIsCapturing }
 }
 
 export default useWebcam
